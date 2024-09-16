3479ba1d8308211d935e6179a34ab3304bbd78fe
Juan Esteban Mesa <practicante.si2@comware.com.co>
2024-09-16 17:10:31 -0500

    feat(Init): New MVC Libro
    :sparkles: a new model view controller for library books has been added.

---
+++ models/Libro.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "libros".
+ *
+ * @property int $id
+ * @property string $titulo
+ * @property string $imagen
+ */
+class Libro extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'libros';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['titulo', 'imagen'], 'required'],
+            [['titulo'], 'string', 'max' => 255],
+            [['imagen'], 'string', 'max' => 2500],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'titulo' => 'Titulo',
+            'imagen' => 'Imagen',
+        ];
+    }
+}
